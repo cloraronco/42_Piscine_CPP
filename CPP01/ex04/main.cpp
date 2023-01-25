65409d48c1cca077e93b10f4e165d86ef5154b89
Cyrille Lora-ronco <clora-ro@z4r1p7.42lyon.fr>
2023-01-25 11:38:59 +0100

    CPP04/ex04 OK

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -3,26 +3,40 @@
 
 int	main(int ac, char **av) {
 
-	std::string	line;
-	size_t		pos;
-
 	if (ac != 4)
-		return (1);
-	std::string		s1(av[2]);
-	std::ifstream	input(av[1]);
-	std::ofstream	output((std::string) av[1] + "_replace.txt");
-
-
-	if (input && output)
 	{
-		pos = line.find(av[2]);
-		while (getline(input, line))
+		std::cout << "You must have three inputs :  <filename>  <s1>  <s2> ." << std::endl;
+		return (1);
+	}
+
+	std::string		line;
+	size_t			pos;
+
+	std::string		s1(av[2]);
+	std::string		s2(av[3]);
+	std::ifstream	file(av[1]);
+	std::ofstream	new_file((std::string) av[1] + ".replace");
+
+	if (file && new_file)
+	{
+		while (getline(file, line))
 		{
-			line.erase(pos);
-			line.insert();
+			pos = line.find(av[2]);
+			while (pos != std::string::npos)
+			{
+				line.erase(pos, s1.length());
+				line.insert(pos, av[3]);
+				pos = line.find(av[2], pos + s1.length());
+			}
+			new_file << line << std::endl;
 		}
 	}
-	input.close();
-	output.close();
+	else
+	{
+		std::cout << "Files stream error" << std::endl;
+		return (1);
+	}
+	file.close();
+	new_file.close();
 	return (0);
 }
