88122b7dbd13364c1e38c667613eb5b57afd3bc3
Cyrille Lora-ronco <clora-ro@z4r4p7.42lyon.fr>
2023-03-08 11:40:59 +0100

    Push 2

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
+
+void	replace(char *av1, char *av2, char *av3)
+{
+	std::string		line;
+	size_t			pos;
+
+	std::string		s1(av2);
+	std::string		s2(av3);
+	std::ifstream	file(av1);
+	std::ofstream	new_file((std::string(av1) + ".replace").data());
+
+	if (file && new_file)
+	{
+		while (getline(file, line))
+		{
+			pos = line.find(av2);
+			while (pos != std::string::npos)
+			{
+				line.erase(pos, s1.length());
+				line.insert(pos, av3);
+				pos = line.find(av2, pos + s2.length());
+			}
+			new_file << line << std::endl;
+		}
+	}
+	else
+	{
+		file.close();
+		new_file.close();
+		std::cout << "Files stream error" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	file.close();
+	new_file.close();
+}
 
 int	main(int ac, char **av) {
 
@@ -8,35 +44,6 @@ int	main(int ac, char **av) {
 		std::cout << "You must have three inputs :  <filename>  <s1>  <s2> ." << std::endl;
 		return (1);
 	}
-
-	std::string		line;
-	size_t			pos;
-
-	std::string		s1(av[2]);
-	std::string		s2(av[3]);
-	std::ifstream	file(av[1]);
-	std::ofstream	new_file((std::string(av[1]) + ".replace").data());
-
-	if (file && new_file)
-	{
-		while (getline(file, line))
-		{
-			pos = line.find(av[2]);
-			while (pos != std::string::npos)
-			{
-				line.erase(pos, s1.length());
-				line.insert(pos, av[3]);
-				pos = line.find(av[2], pos + s2.length());
-			}
-			new_file << line << std::endl;
-		}
-	}
-	else
-	{
-		std::cout << "Files stream error" << std::endl;
-		return (1);
-	}
-	file.close();
-	new_file.close();
+	replace(av[1], av[2], av[3]);
 	return (0);
 }
