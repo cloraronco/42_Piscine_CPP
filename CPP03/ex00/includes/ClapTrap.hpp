98988995610cad379cd8641ceac42c421a814887
Cyrille Lora-ronco <clora-ro@z3r6p3.42lyon.fr>
2023-03-15 10:48:16 +0100

    CPP03/ex00 OK

--- CPP03/ex00/includes/ClapTrap.hpp
+++ CPP03/ex00/includes/ClapTrap.hpp
@@ -1,5 +1,14 @@
 #pragma once
 #include <iostream>
+#define RESET "\e[0m"
+#define GREY "\e[0;30m"
+#define RED "\e[0;31m"
+#define GREEN "\e[0;32m"
+#define YELLOW "\e[0;33m"
+#define BLUE "\e[0;34m"
+#define PINK "\e[0;35m"
+#define MAGENTA "\e[0;36m"
+#define WHITE "\e[0;37m"
 
 class ClapTrap
 {
--- CPP03/ex00/srcs/main.cpp
+++ CPP03/ex00/srcs/main.cpp
@@ -2,53 +2,62 @@
 
 /*____________Not enough hitPoints___________*/
 
+int main(void)
+{
+	ClapTrap	Player("Macron");
+	ClapTrap	Melenchon(Player);
+	
+	ClapTrap	NoName;
+
+	Player.setDamage(3);
+	Player.takeDamage(4);
+	Player.beRepaired(2);
+	Player.takeDamage(4);
+	Player.takeDamage(4);
+
+	NoName.takeDamage(2);
+
+	return (0);
+}
+
+
+/*___________Not enough energyPoints__________*/
+
 // int main(void)
 // {
 // 	ClapTrap	Player("Macron");
 // 	ClapTrap	Melenchon(Player);
-	
+
 // 	ClapTrap	NoName;
 
 // 	Player.setDamage(3);
+// 	Player.attack("Melenchon");
+// 	Player.attack("Melenchon");
 // 	Player.takeDamage(4);
 // 	Player.beRepaired(2);
-// 	Player.takeDamage(4);
-// 	Player.takeDamage(4);
+// 	Player.attack("Melenchon");
+// 	Player.attack("Melenchon");
+// 	Player.attack("Melenchon");
+// 	Player.attack("Melenchon");
+// 	Player.attack("Melenchon");
+// 	Player.attack("Melenchon");
+// 	Player.attack("Melenchon");
+// 	Player.attack("Melenchon");
+// 	Player.beRepaired(2);
 
 // 	NoName.takeDamage(2);
 
 // 	return (0);
 // }
 
-
-/*___________Not enough energyPoints__________*/
-
-int main(void)
-{
-	ClapTrap	Player("Macron");
-
-	Player.setDamage(3);
-	Player.attack("Melenchon");
-	Player.attack("Melenchon");
-	Player.takeDamage(4);
-	Player.beRepaired(2);
-	Player.attack("Melenchon");
-	Player.attack("Melenchon");
-	Player.attack("Melenchon");
-	Player.attack("Melenchon");
-	Player.attack("Melenchon");
-	Player.attack("Melenchon");
-	Player.attack("Melenchon");
-	Player.attack("Melenchon");
-	Player.beRepaired(2);
-	return (0);
-}
-
 /*_____________________Dead___________________*/
 
 // int main(void)
 // {
 // 	ClapTrap	Player("Macron");
+// 	ClapTrap	Melenchon(Player);
+
+// 	ClapTrap	NoName;
 
 // 	Player.setDamage(3);
 // 	Player.attack("Melenchon");
@@ -59,5 +68,8 @@ int main(void)
 // 	Player.takeDamage(8);
 // 	Player.beRepaired(2);
 // 	Player.attack("Melenchon");
+
+// 	NoName.takeDamage(2);
+
 // 	return (0);
 // }
