30a8327960b0abbd0a70f5339160e8ef095c187a
Cyrille Lora-ronco <clora-ro@z3r6p3.42lyon.fr>
2023-03-15 12:50:05 +0100

    CPP03 OK

--- CPP03/ex02/srcs/main.cpp
+++ CPP03/ex02/srcs/main.cpp
@@ -3,33 +3,33 @@
 
 /*____________Not enough hitPoints___________*/
 
-// int main(void)
-// {
-// 	ClapTrap	Emmanuel("Macron");
-// 	ClapTrap	Player1 = Emmanuel;
+int main(void)
+{
+	ClapTrap	Emmanuel("Macron");
+	ClapTrap	Player1 = Emmanuel;
 
-// 	Emmanuel.setDamage(3);
-// 	Emmanuel.attack("Melenchon");
-// 	Emmanuel.takeDamage(10);
+	Emmanuel.setDamage(3);
+	Emmanuel.attack("Melenchon");
+	Emmanuel.takeDamage(10);
 
 
-// 	ScavTrap	JeanLuc("Melenchon");
-// 	ScavTrap	Player2 = JeanLuc;
+	ScavTrap	JeanLuc("Melenchon");
+	ScavTrap	Player2 = JeanLuc;
 
-// 	JeanLuc.guardGate();
-// 	JeanLuc.attack("Macron");
-// 	JeanLuc.beRepaired(2);
-// 	JeanLuc.takeDamage(102);
-// 	JeanLuc.guardGate();
+	JeanLuc.guardGate();
+	JeanLuc.attack("Macron");
+	JeanLuc.beRepaired(2);
+	JeanLuc.takeDamage(102);
+	JeanLuc.guardGate();
 
-// 	FragTrap	Christine("Lagarde");
-// 	FragTrap	Player3 = Christine;
+	FragTrap	Christine("Lagarde");
+	FragTrap	Player3 = Christine;
 
-// 	Christine.highFivesGuys();
-// 	Christine.takeDamage(999999);
+	Christine.highFivesGuys();
+	Christine.takeDamage(999999);
 
-// 	return (0);
-// }
+	return (0);
+}
 
 
 /*___________Not enough energyPoints__________*/
@@ -53,20 +53,20 @@
 
 /*_____________________Dead___________________*/
 
-int main(void)
-{
-	FragTrap	Christine("Lagarde");
-	FragTrap	Player1 = Christine;
+// int main(void)
+// {
+// 	FragTrap	Christine("Lagarde");
+// 	FragTrap	Player1 = Christine;
 
-	Player1.setDamage(3);
-	Player1.attack("Melenchon");
-	Player1.attack("Melenchon");
-	Player1.takeDamage(70);
-	Player1.beRepaired(20);
-	Player1.takeDamage(50);
+// 	Player1.setDamage(3);
+// 	Player1.attack("Melenchon");
+// 	Player1.attack("Melenchon");
+// 	Player1.takeDamage(70);
+// 	Player1.beRepaired(20);
+// 	Player1.takeDamage(50);
 
-	Player1.takeDamage(1);
-	Player1.beRepaired(2);
-	Player1.attack("Melenchon");
-	return (0);
-}
+// 	Player1.takeDamage(1);
+// 	Player1.beRepaired(2);
+// 	Player1.attack("Melenchon");
+// 	return (0);
+// }
